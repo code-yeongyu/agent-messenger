e2bcbec673d44c47397aaa5c940bfff4be6ea1ea
Suyeol Jeon <devxoul@gmail.com>
2026-01-31 13:24:13 +0900

    feat(teams): add CLI entry point and register binary

--- src/platforms/teams/cli.ts
+++ src/platforms/teams/cli.ts
@@ -1,5 +1,36 @@
-/**
- * CLI entry point for agent-teams command
- */
+#!/usr/bin/env bun
 
-// TODO: Implement following src/platforms/discord/cli.ts pattern
+import { Command } from 'commander'
+import pkg from '../../../package.json'
+import {
+  authCommand,
+  channelCommand,
+  fileCommand,
+  messageCommand,
+  reactionCommand,
+  snapshotCommand,
+  teamCommand,
+  userCommand,
+} from './commands'
+
+const program = new Command()
+
+program
+  .name('agent-teams')
+  .description('CLI tool for Microsoft Teams communication')
+  .version(pkg.version)
+  .option('--pretty', 'Pretty-print JSON output')
+  .option('--team <id>', 'Use specific team')
+
+program.addCommand(authCommand)
+program.addCommand(teamCommand)
+program.addCommand(channelCommand)
+program.addCommand(fileCommand)
+program.addCommand(messageCommand)
+program.addCommand(reactionCommand)
+program.addCommand(snapshotCommand)
+program.addCommand(userCommand)
+
+program.parse(process.argv)
+
+export default program
