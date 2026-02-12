fdb73d38e57d740a6ad1d2ee5a7e0109e382a1fe
Jeon Suyeol <devxoul@gmail.com>
2026-02-12 17:35:18 +0900

    Add logo icon to docs

--- docs/src/app/layout.config.tsx
+++ docs/src/app/layout.config.tsx
@@ -1,7 +1,14 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared'
+import Image from 'next/image'
+import icon from './icon.png'
 
 export const baseOptions: BaseLayoutProps = {
   nav: {
-    title: 'Agent Messenger',
+    title: (
+      <>
+        <Image src={icon} alt="Agent Messenger" width={24} height={24} />
+        Agent Messenger
+      </>
+    ),
   },
 }
