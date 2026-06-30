e618fa024bb1d54d7be537a8c7615a1affaa5a15
Jeon Suyeol <devxoul@gmail.com>
2026-06-30 11:45:27 +0900

    feat(instagram): export realtime and hybrid listeners from SDK

--- src/platforms/instagram/index.ts
+++ src/platforms/instagram/index.ts
@@ -1,6 +1,16 @@
 export { InstagramClient } from './client'
 export { InstagramCredentialManager } from './credential-manager'
+export {
+  InstagramHybridListener,
+  type InstagramHybridListenerEventMap,
+  type InstagramHybridListenerOptions,
+} from './hybrid-listener'
 export { InstagramListener, type InstagramListenerEventMap } from './listener'
+export {
+  InstagramRealtimeListener,
+  type InstagramRealtimeListenerEventMap,
+  type InstagramRealtimeListenerOptions,
+} from './realtime-listener'
 export { InstagramTokenExtractor, type ExtractedInstagramCookies } from './token-extractor'
 export {
   createAccountId,
